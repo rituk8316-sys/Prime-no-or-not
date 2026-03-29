cc35f677123035387327325e5746e2a9b969b1eb
rituk8316-sys <rituk8316@gmail.com>
2026-03-29 16:39:17 +0530

    Create Prime number or not.py

---
+++ Prime number or not.py
@@ -0,0 +1,19 @@
+# Prime Number Check
+
+## Problem
+Write a Python program to check whether a number is prime or not.
+
+## Solution (Python Code)
+
+```python
+num = int(input("Enter a number: "))
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
